e188b88bc7a0f69a0daddd0caf7394fa051b17ec
deJav1u <73663899+alex-nazaretyan@users.noreply.github.com>
2021-01-12 08:43:09 +0200

    HTML and CSS
    small projects for fixing

---
+++ Small HTML and CSS projects/Whiteboard/js.js
@@ -0,0 +1,14 @@
+function solve (kurwa) {
+    if (kurwa == true) {
+        for(let i = 0; i <=10; i++){
+            console.log(i);
+
+        }
+    }else{
+        console.log('opalalqnkaaaaaaa');
+    }
+    
+    
+}
+
+solve(false);
